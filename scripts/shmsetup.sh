b33235b062739258e9c743e9fe63d6c242c28a76
Rene Romero Benavides <rene.romero.b@gmail.com>
2013-05-13 13:34:59 -0500

    Shared memory setup file

---
+++ scripts/shmsetup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+page_size=$(getconf PAGE_SIZE)
+phys_pages=$(getconf _PHYS_PAGES)
+shmall=$(expr $phys_pages / 2)
+shmmax=$(expr $shmall \* $page_size)
+echo kernel.shmmax = $shmmax
+echo kernel.shmall = $shmall
