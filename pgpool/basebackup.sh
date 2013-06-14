2fdbfcb16dc6743ab8afa0c1254591135dc06270
John Anderson <sontek@gmail.com>
2013-06-14 04:20:04 -0500

    pgpool fixes

--- pgpool/basebackup.sh
+++ pgpool/basebackup.sh
@@ -3,11 +3,12 @@
 SOURCE_CLUSTER=$1
 DEST_HOST=$2
 DEST_CLUSTER=$3
+
 PORT=5432
 
 psql -p $PORT -c "SELECT pg_start_backup('Streaming Replication', true)" postgres
 
-rsync -C -a -c --delete --exclude postgresql.conf --exclude postmaster.pid \
+rsync -C -a -c --delete --exclude postmaster.pid \
 --exclude postmaster.opts --exclude pg_log \
 --exclude recovery.conf --exclude recovery.done \
 --exclude pg_xlog \
@@ -15,7 +16,6 @@ $SOURCE_CLUSTER/ $DEST_CLUSTER/
 
 mkdir $DEST_CLUSTER/pg_xlog
 mkdir $DEST_CLUSTER/pg_log
-
 chmod -R 700 $DEST_CLUSTER/pg_xlog
 test -f $DEST_CLUSTER/recovery.done && rm $DEST_CLUSTER/recovery.done
 cat > $DEST_CLUSTER/recovery.conf <<EOF
